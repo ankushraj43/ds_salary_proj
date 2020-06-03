c751d73f95050b29503dcfede5593d103b1176b4
ankushraj43 <ankushraj43@gmail.com>
2020-06-03 12:27:26 +0530

    Created the flask API endpoint

--- model_building.py
+++ model_building.py
@@ -90,3 +90,19 @@ mean_absolute_error(y_test,tpred_lml)
 mean_absolute_error(y_test,tpred_rf)
 
 mean_absolute_error(y_test,(tpred_lm+tpred_rf)/2)
+
+
+import pickle
+pickl = {'model': gs.best_estimator_}
+pickle.dump( pickl, open( 'model_file' + ".p", "wb" ) )
+
+file_name = "model_file.p"
+with open(file_name, 'rb') as pickled:
+    data = pickle.load(pickled)
+    model = data['model']
+
+(X_test.iloc[1,:]).values
+model.predict((X_test.iloc[1,:]).values.reshape(1,-1))
+
+list(X_test.iloc[1,:])
+model.predict(np.array(list(X_test.iloc[1,:])).reshape(1,-1))[0]
